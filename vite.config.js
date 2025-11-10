6d3052d082c2bf035797210296002dfc3efc92fd
Isabel Romeo <isabeldxn99@gmail.com>
2025-11-10 06:14:40 +0100

    vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 import jsconfigPaths from "vite-jsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 1000, // Opcional: aumenta el límite de warning
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
